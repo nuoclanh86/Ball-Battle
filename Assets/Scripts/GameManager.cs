31b66f3ef09c829c88f4e20f60b02ab458a932cf
nuoclanhcode <nuoclanh86@gmail.com>
2020-10-13 14:53:54 +0700

    - add some example scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// enum GameStates
+// {
+    // Idle,
+    // Playing,
+    // Lose,
+    // Win
+// }
+
+// public enum IndexGameColor
+// {
+    // black,
+    // white
+// }
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
