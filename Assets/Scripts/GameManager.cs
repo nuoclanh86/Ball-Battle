ae0bf5df6d88ea5e31b28a2b88ab359e61ca95ab
nuoclanhcode <nuoclanh86@gmail.com>
2020-10-14 11:21:08 +0700

    - spawn a random ball in attacker's field

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,12 +12,19 @@ enum GameStates
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject battleFieldPrefab;
     public GameObject soldierPrefab;
+    public GameObject theBallPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject wallBot = battleFieldPrefab.transform.GetChild(0).transform.GetChild(0).gameObject;
+        GameObject wallRight = battleFieldPrefab.transform.GetChild(0).transform.GetChild(3).gameObject;
+        float deltaRange = 0.5f; // do not spawn too nearly walls
+        float ran_x = Random.Range(-wallRight.transform.position.x + deltaRange, wallRight.transform.position.x - deltaRange);
+        float ran_z = Random.Range(wallBot.transform.position.z + deltaRange, 0.0f - deltaRange);
+        Instantiate(theBallPrefab, new Vector3(ran_x, 0.5f, ran_z), Quaternion.identity);
     }
 
     // Update is called once per frame
